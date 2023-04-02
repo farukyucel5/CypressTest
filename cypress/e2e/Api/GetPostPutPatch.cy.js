1ea87131f2c5f651d4190bcabf4cf4cc47bbc222
farukyucel5 <117035308+farukyucel5@users.noreply.github.com>
2023-04-03 00:14:31 +0300

    03.03.2023 Faruk updating

--- cypress/e2e/Api/GetPostPutPatch.cy.js
+++ cypress/e2e/Api/GetPostPutPatch.cy.js
@@ -2,19 +2,19 @@
 
 describe("get request", () => {
 
-    it("https://gorest.co.in/public/v2/users/665620",() =>{
+    it("https://gorest.co.in/public/v2/users/x",() =>{
 
-    cy.request({
+       cy.request({
         method : "GET",
-        url: "https://gorest.co.in/public/v2/users/665620",
+        url: "https://gorest.co.in/public/v2/users/668611",
         Headers:{
             'authorization':"Bearer f057f4dd41b1f826a534feb56c0ae6e1e7b869dd681f7ba3314a0db2104c5c96"
         }
 
-    }).then((res)=>{
-        expect(res.status).to.eq(200)
-        expect(res.body.gender).to.eq("female")
-        expect(res.body.status).to.eq("inactive")
+       }).then((res)=>{
+           expect(res.status).to.eq(200)
+           expect(res.body.gender).to.eq("male")
+           expect(res.body.status).to.eq("inactive")
 
     })
 
@@ -22,16 +22,25 @@ describe("get request", () => {
     })
 
     it("https://gorest.co.in/public/v2/users",()=>{
-
-   cy.request({
-        method : "GET",
-        url: "https://gorest.co.in/public/v2/users",
-        Headers:{
-            'authorization':"Bearer f057f4dd41b1f826a534feb56c0ae6e1e7b869dd681f7ba3314a0db2104c5c96"
+        let expectedBody={
+            "id":668611,
+            "name": "Ganaka Khatri",
+            "email":"khatri_ganaka@hettinger-braun.io",
+            "gender":"male",
+            "status": "inactive"
         }
 
+        cy.request({
+          method : "GET",
+          url: "https://gorest.co.in/public/v2/users",
+          Headers:{
+            'authorization':"Bearer f057f4dd41b1f826a534feb56c0ae6e1e7b869dd681f7ba3314a0db2104c5c96"
+          }
+
     }).then((res)=>{
-        //expect(res.body).to.have.lengthOf(10)
+        expect(res.body).to.have.lengthOf(10)
+        const actualArray = res.body;
+        expect(actualArray).to.deep.include(expectedBody);
         res.body.forEach(element => {
             expect(element.id).to.be.greaterThan(600000)
         });
